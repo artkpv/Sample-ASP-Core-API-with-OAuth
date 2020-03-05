ea23a335bfd1a5879177c2d6459913841ddcbf10
AK <artyomkarpov@gmail.com>
2020-03-05 12:58:34 +0300

    Demo: adding, getting, modifying

--- BA.CliClient/run.py
+++ BA.CliClient/run.py
@@ -1,10 +1,8 @@
 #!/bin/python3
 
-import requests
-import os
-from oauthlib.oauth2 import BackendApplicationClient, LegacyApplicationClient
-from requests_oauthlib import OAuth2Session
 from context import session as contextsession
+from datetime import datetime, timezone
+import json
 
 
 def main():
@@ -12,7 +10,7 @@ def main():
     while True:
         actiontype = choose_action()
         if actiontype is ExitAction:
-            print('exit')
+            print('Demo exit')
             return
         print("Action chosen: " + actiontype.description)
         actiontype(context).run()
@@ -35,8 +33,10 @@ def choose_action():
 
 class Action(object):
     description = "<Unknown>"
+
     def __init__(self, context):
         self.context = context
+        self.session = self.context.get_session_by_user_pass
 
     def run(self):
         print("No action")
@@ -50,16 +50,83 @@ class GetBikingAction(Action):
     description = "Get biking entries"
 
     def run(self):
-        self.context.get_session_by_user_pass()
-        print('getting entries')
+        api = self.session()
+        page = int(input("Page: ") or 0)
+        pagesize = int(input("Page size: ") or 0)
+        gparams = {}
+        if page and pagesize:
+            gparams['page'] = page
+            gparams['pagesize'] = pagesize
+        filter_ = input("Filter: ").strip() or None 
+        if filter_:
+            gparams['filter'] = filter_
+
+        gr = api.get(api.bikingentryurl, params=gparams)
+
+        assert gr.status_code == 200
+
+        entries = gr.json()
+
+        print("Found {} entries".format(len(entries)))
+
+        for i, entry in enumerate(entries):
+            print("Entry 1:".format(i+1))
+            print_bikingentry(entry)
 
 
 class RecordBikingAction(Action):
     description = "Record biking entry"
 
+    def run(self):
+        api = self.session()
+
+        entry = {
+            "startTime": None,
+            "distanceMeters": int(input('Distance in meters: ')),
+            "durationSeconds": int(input('Duration in seconds: ')), 
+            "latitude": 51.48,
+            "longitude": 0.0
+        }
+
+        pr = api.post(api.bikingentryurl,
+                headers={'Content-Type': 'application/json'},
+                data=json.dumps(entry))
+        
+        assert pr.status_code == 201
+
+        print("Entry recorded: " + str(pr.json()))
+
 
 class ModifyBikingAction(Action):
     description = "Modify biking entry"
+
+    def run(self):
+        api = self.session()
+
+        entryid = int(input("Enter entry id: "))
+
+        gr = api.get(api.bikingentryurl + '/' + str(entryid))
+        if gr.status_code != 200:
+            print("Entry not found")
+            return
+        entry = gr.json()
+
+        print("Found entry:")
+        print_bikingentry(entry)
+
+        entry['distanceMeters'] = int(input('Distance in meters: ') or entry['distanceMeters'])
+        entry["durationSeconds"] = int(input('Duration in seconds: ') or entry["durationSeconds"])
+        entry["latitude"] = int(input('Latitude: ') or entry["latitude"])
+        entry["longitude"] = int(input('Longitude: ') or entry["longitude"]) 
+
+        pr = api.put(api.bikingentryurl + '/' + str(entryid),
+                headers={'Content-Type': 'application/json'},
+                data=json.dumps(entry))
+
+        assert pr.status_code == 204
+
+        print("Entry modified")
+
 
 
 class DeleteBikingAction(Action):
@@ -87,6 +154,19 @@ class ClientContext(object):
 
     def _create_session(self):
         return  contextsession()
+
+
+def print_bikingentry(entry):
+    def getprefix(s):
+        if isinstance(s, str):
+            prefixlen = 100
+            if len(s) > prefixlen:
+                return s[:prefixlen] + "..."
+        return s
+
+    for k in entry:
+        print("\t{} {}".format(k, getprefix(entry[k])))
+
 
 
 if __name__ == '__main__':
